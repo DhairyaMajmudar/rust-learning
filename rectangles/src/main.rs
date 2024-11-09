1ed565be1f9c162e84a04d6ae6c5e092f69fc72c
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2024-11-09 12:28:59 +0530

    learning method syntax

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -9,6 +9,16 @@ struct Rectangle {
     width: u32,
 }
 
+// method syntax https://doc.rust-lang.org/book/ch05-03-method-syntax.html#method-syntax
+// these are smiliar to functions but are defined within the context of structs
+// their first parameter is always self
+
+impl Rectangle {
+    fn cal_area(&self) -> u32 {
+        self.height * self.width
+    }
+}
+
 fn main() {
     // inistance of rectangle
     let rect1 = Rectangle {
@@ -21,6 +31,9 @@ fn main() {
 
     println!("{:?}", rect1); // priting rectangle
     println!("{:#?}", rect1); // pretty priting rectagle
+
+    // learning method syntax
+    println!("area obtained through method {}", rect1.cal_area());
 }
 
 fn area(dimensions: &Rectangle) -> u32 {
